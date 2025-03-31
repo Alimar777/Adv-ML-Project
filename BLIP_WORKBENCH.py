02e6a53a8ef9fca5b367fcac13a04ae44d7dd2d0
Matthew Rackley <alimar777@gmail.com>
2025-03-30 22:25:43 -0600

    update to blipworkbench

--- BLIP_WORKBENCH.py
+++ BLIP_WORKBENCH.py
@@ -2,7 +2,8 @@ import os
 import time
 import statistics
 from config import *
-from utils import extract_frames, generate_caption, summarize_captions, rephrase_summary, save_results, print_summary
+from utils import extract_frames, generate_caption, save_results, print_summary
+from rolling_windows import RollingWindowManager
 
 # Process videos 01.mp4 to 10.mp4
 for i in range(1, 11):
@@ -21,6 +22,7 @@ for i in range(1, 11):
     frames, total_frames, video_duration = extract_frames(video_path, FRAME_INTERVAL)
     captions, csv_data, frame_times = [], [], []
     total_start_time = time.time()
+    window_manager = RollingWindowManager()
 
     for frame_number, frame in frames:
         start_time = time.time()
@@ -31,13 +33,17 @@ for i in range(1, 11):
         frame_times.append(end_time - start_time)
         csv_data.append([frame_number, f"frame_{frame_number}.jpg", caption, end_time - start_time])
 
+        # Update rolling windows
+        window_manager.update(caption, frame_number)
+
     mean_time = statistics.mean(frame_times)
     median_time = statistics.median(frame_times)
     mode_time = statistics.mode(frame_times) if len(frame_times) > 1 else frame_times[0]
 
-    # Summarize and rephrase captions
-    summary = summarize_captions(captions, summarizer)
-    final_summary = rephrase_summary(summary, rephraser)
+    final_summary = window_manager.get_scene_summary()
+    transition_pair = window_manager.get_transition_text()
+    noise_common = window_manager.get_common_noise()
+
     total_end_time = time.time()
     total_processing_time = total_end_time - total_start_time
 
@@ -48,7 +54,9 @@ for i in range(1, 11):
         "Median Processing Time (s)": median_time,
         "Mode Processing Time (s)": mode_time,
         "Total Processing Time (s)": total_processing_time,
-        "Summary": final_summary
+        "Scene Summary": final_summary,
+        "Transition Pair": transition_pair,
+        "Common Noise Caption": noise_common
     }
 
     save_results(video_filename, video_output_dir, csv_data, total_stats)
